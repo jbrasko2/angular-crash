ee8a7434da76af02cc2aae7e76988d1e2d290b7a
jbrasko <jbrasko0508@gmail.com>
2021-10-04 13:18:14 -0400

    set up http

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Task } from '../Task';
 import { TASKS } from '../mock-tasks';
@@ -7,7 +8,9 @@ import { TASKS } from '../mock-tasks';
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
     const tasks = of(TASKS);
